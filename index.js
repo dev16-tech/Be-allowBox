f90251d310ebeb0b04b0cab5f2092d8014173bdc
dev16-tech <dev16.tech@gmail.com>
2025-06-02 23:49:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = 8000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node!\n'
+  const msg = 'Hello Node! cicdis setup\n'
   res.end(msg);
 });
 
